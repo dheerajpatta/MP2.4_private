5bf6e0dd3246c99b47bc9d2d500ddd3f9d6ea358
Dheeraj Patta <dheeraj.patta@yahoo.com>
2020-10-07 00:01:56 -0400

    Added PL2Ranker to the script

--- search_eval.py
+++ search_eval.py
@@ -4,6 +4,48 @@ import time
 import metapy
 import pytoml
 
+class InL2Ranker(metapy.index.RankingFunction):
+    """
+    Create a new ranking function in Python that can be used in MeTA.
+    """
+    def __init__(self, some_param=0.5):
+        self.c = some_param
+        # You *must* call the base class constructor here!
+        super(InL2Ranker, self).__init__()
+
+    def score_one(self, sd):
+        """
+        Scoring function for current Query (Q) and doucment (D)
+        sd : score data object
+        N : the total number of documents in the corpus C
+        avgdl : the average document length
+        c > 0 : is a parameter
+        """
+        tfn = sd.doc_term_count * math.log(1.0 + (sd.avg_dl/sd.doc_size),2)
+        
+        tfn_by_tfn_plus_c = sd.query_term_weight * (tfn/(tfn+self.c))
+        log_n_plus_one_by_corpus = math.log((sd.num_docs+1)/(sd.corpus_term_count + 0.5),2)
+        
+        return (tfn_by_tfn_plus_c * log_n_plus_one_by_corpus)
+
+class PL2Ranker(metapy.index.RankingFunction):
+    """
+    Create a new ranking function in Python that can be used in MeTA
+    """
+    def __init__(self, c_param=0.5):
+        self.c = c_param
+        super(PL2Ranker, self).__init__()
+
+    def score_one(self, sd):
+        lda = sd.num_docs / sd.corpus_term_count
+        tfn = sd.doc_term_count * math.log2(1.0 + self.c * sd.avg_dl /
+                sd.doc_size)
+        if lda < 1 or tfn <= 0:
+            return 0.0
+        numerator = tfn * math.log2(tfn * lda) \
+                        + math.log2(math.e) * (1.0 / lda - tfn) \
+                        + 0.5 * math.log2(2.0 * math.pi * tfn)
+        return sd.query_term_weight * numerator / (tfn + 1.0)
 
 def load_ranker(cfg_file):
     """
@@ -11,7 +53,9 @@ def load_ranker(cfg_file):
     The parameter to this function, cfg_file, is the path to a
     configuration file used to load the index.
     """
-    return metapy.index.OkapiBM25(k1=1.2,b=0.75,k3=7.0)
+    # return metapy.index.OkapiBM25(k1=1.2,b=0.75,k3=7.0)
+    # return InL2Ranker(some_param=0.5)
+    return PL2Ranker(c_param=0.5)
 
 if __name__ == '__main__':
     # if len(sys.argv) != 2:
